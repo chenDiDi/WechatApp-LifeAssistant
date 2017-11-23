1759de6d3143f0886d011fe7e7dc66c74d644773
didichen <757960181@qq.com>
2017-11-23 21:43:14 +0800

    [markers增加多点标注+真机测试效果+polyline线路实现]

--- pages/location/location.js
+++ pages/location/location.js
@@ -1,37 +1,58 @@
 Page({
   data: {
-    latitude: 0,
-    longitude: 0,
-    speed: 0,
+    latitude: 22.933542,
+    longitude: 113.89537,
+    speed: -1,
     startLocation: {
       latitude: 23.099994,
       longitude: 113.324520,
     },
     markers: [{
-      callout: {
-        bgColor: '#ccc',
-        content: '实验室',
-      },
-      iconPath: "/resources/anchor.png",
-      id: 0,
-      latitude: 0,
-      longitude: 0,
+      iconPath: "/images/location/anchor.png",
+      id: 1,
+      title: '1',
+      latitude: 22.933542,
+      longitude: 113.89537,
+      width: 30,
+      height: 30
+    }, {
+        iconPath: "/images/location/anchor.png",
+      id: 2,
+      title: '2',
+      latitude: 22.932742,
+      longitude: 113.89837,
+      width: 30,
+      height: 30
+    }, {
+        iconPath: "/images/location/anchor.png",
+      id: 3,
+      title: '3',
+      latitude: 22.934742,
+      longitude: 113.90737,
+      width: 30,
+      height: 30
+    }, {
+        iconPath: "/images/location/anchor.png",
+      id: 4,
+      title: '4',
+      latitude: 22.944742,
+      longitude: 113.90737,
       width: 30,
       height: 30
     }],
     polyline: [{
       points: [{
-        longitude: 113.751765,
-        latitude: 23.020536
+        latitude: 22.933542,
+        longitude: 113.89537,
       }, {
-        longitude: 113.761765,
-        latitude: 23.020536
+        latitude: 22.932742,
+        longitude: 113.89837,
       }, {
-        longitude: 113.762343,
-        latitude: 23.035536
+        latitude: 22.934742,
+        longitude: 113.90737,
       }, {
-        longitude: 113.763434,
-        latitude: 23.040536
+        latitude: 22.944742,
+        longitude: 113.90737,
       }],
       color: "#FF0000DD",
       width: 4,
@@ -43,7 +64,7 @@ Page({
     this.mapCtx = wx.createMapContext('map');
   },
   onLoad: function() {
-    this._currentLocation();
+    // this._currentLocation();
   },
   regionchange(e) {
     console.log(e.type, '视野发生变化时触发')
@@ -60,26 +81,28 @@ Page({
     wx.getLocation({
       type: 'gcj02',
       success: function (res) {
-        console.log(res, 'res00-----');
+        const {markers} = _this.data;
+        console.log(markers, markers);
+        markers.push({
+          iconPath: "/images/location/anchor.png",
+          id: 0,
+          title: '0',
+          latitude: res.latitude,
+          longitude: res.longitude,
+          width: 30,
+          height: 30
+        });
         _this.setData({
           latitude: res.latitude,
           longitude: res.longitude,
           speed: res.speed,
-          markers: [{
-            id: 1,
-            latitude: res.latitude,
-            longitude: res.longitude,
-            speed: res.speed,
-            accuracy: res.accuracy,
-            width: 30,
-            height: 30,
-            title: "当前位置"
-          }],
           startLocation: {
             latitude: res.latitude,
             longitude: res.longitude,
           },
+          markers,
         })
+        console.log(res, 'res00-----');                  
       }
     })
   },
